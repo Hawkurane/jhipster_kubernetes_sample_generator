09da57db45daa687fba935810473153382b624b0
Jérôme PHAM <hawkurane@gmail.com>
2019-10-15 11:32:44 +0200

    generated files now up to date with daily build schemes

--- src/yml-generator.js
+++ src/yml-generator.js
@@ -4,27 +4,38 @@ const ejs = require("ejs");
 const fs = require("fs");
 const path = require("path");
 try {
+  config.prodDatabaseType.forEach(prodDbType => {
     config.cacheProvider.forEach(cacheType => {
-        ejs.renderFile(
-          "templates/azure-pipelines-jdl-app.yaml.ejs",
-          { config: config, cacheType: cacheType },
-          function(err, str) {
-            if (err) throw new Error(err);
-            fs.writeFile(
-              path.join(
-                __dirname,
-                `../yamls/azure-pipelines-jdl-app-${cacheType}.yaml`
-              ),
-              str,
-              function(error, data) {
-                console.log(`... azurepipelines-jdl-app-${cacheType}.yaml`);
-                if (error) throw err;
-              }
-            );
-          }
-        );
-      });
-      console.log(chalk.green("====> Yamls generated successfully."));
-} catch (e){
-    console.error(chalk.red(e));
+      ejs.renderFile(
+        "templates/azure-pipelines-jdl-app.yaml.ejs",
+        { config: config, cacheType: cacheType, prodDbType: prodDbType },
+        function(err, str) {
+          if (err) throw new Error(err);
+          fs.mkdirSync(
+            path.join(__dirname, `../yamls/`),
+            { recursive: true },
+            err => {
+              if (err) throw new Error(err);
+            }
+          );
+          fs.writeFile(
+            path.join(
+              __dirname,
+              `../yamls/azure-pipelines-jdl-app-${cacheType}-${prodDbType}.yaml`
+            ),
+            str,
+            function(error, data) {
+              console.log(
+                `... azurepipelines-jdl-app-${cacheType}-${prodDbType}.yaml`
+              );
+              if (error) throw err;
+            }
+          );
+        }
+      );
+    });
+  });
+  console.log(chalk.green("====> Yamls generated successfully."));
+} catch (e) {
+  console.error(chalk.red(e));
 }
